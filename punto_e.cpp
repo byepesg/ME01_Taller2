0df2dd26ce73996d00a22bf99fbed7c5e2f4f0d1
hectorhernandezalfonso <halfonsoh@unal.edu.co>
2025-02-19 00:12:07 -0500

    added lcgrand

--- punto_e.cpp
+++ punto_e.cpp
@@ -1,67 +1,49 @@
+
+
 /*
 
+PARA USAR ESTO
 
-PARA EJECUTAR
-
-1. g++ -o punto_e punto_e.cpp -lm
-2. Editar params.txt
-3. ./punto_e
-4. visualice el resultado en result.txt
-
-
-
-el params.txt  se ve como
-a b c d e
-
-
-con la forma 
-a = tiempo_entre_llegadas  
-b = tiempo_servicio  
-c = numero_clientes  
-d = numero_servidores  
-e = tipo_simulacion (0 para erlang b y 1 para erlang c)
-
-
+1. g++ -o punto_e punto_e.cpp lcgrand.c -lm
+2. ./punto_e
+3. revisar result.txt
 
 */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include "lcgrand.h"  
 
-#define LIMITE_COLA    1000         /* Tamaño máximo de la cola de espera (para Erlang C) */
-#define INFINITY_TIME  1.0e+30      /* Un número muy grande para representar "infinito" */
+#define LIMITE_COLA    1000         /* Tamaño máximo de la cola (para Erlang C) */
+#define INFINITY_TIME  1.0e+30      /* Representa "infinito" */
 #define EVENTO_LLEGADA  1
 #define EVENTO_SALIDA   2
 
 /* Variables globales */
-int tipo_simulacion;              // Tipo de simulación: 0 = Erlang B; 1 = Erlang C.
+int tipo_simulacion;              // 0 = Erlang B; 1 = Erlang C
 int num_servidores;               // Número de servidores
 double t_interarribo_promedio;    // Tiempo medio entre llegadas
 double t_servicio_promedio;       // Tiempo medio de servicio
 int num_llegadas_requeridas;      // Número de llegadas a simular
 
 int total_llegadas;             // Total de llegadas procesadas
-int llamadas_atendidas;         // Número de llamadas atendidas
-int llamadas_bloqueadas;        // (Erlang B) Número de llamadas bloqueadas
-int llamadas_esperaron;         // (Erlang C) Número de llamadas que esperaron
-double tiempo_total_espera;     // Tiempo total de espera de las llamadas que esperaron
-
+int llamadas_atendidas;         // Llamadas atendidas
+int llamadas_bloqueadas;        // (Erlang B) Llamadas bloqueadas
+int llamadas_esperaron;         // (Erlang C) Llamadas que esperaron
+double tiempo_total_espera;     // Tiempo total de espera
 double tiempo_simulacion;       // Reloj de simulación
-double area_ocupada;            // Tiempo acumulado de servidores ocupados (para estadísticas de promedio en el tiempo)
+double area_ocupada;            // Tiempo acumulado de servidores ocupados
 
 /* Para la programación de eventos (Erlang C) */
 double proxima_llegada;         // Tiempo de la próxima llegada
 double *proxima_salida;         // Arreglo de tiempos de próxima salida (uno por servidor)
 
-/* Para la simulación de Erlang B (sistema de pérdida) usamos:
-   - un contador de servidores ocupados actual
-   - un arreglo (proxima_salida) para cada servidor ocupado (se establece en INFINITY_TIME cuando está libre)
-*/
+/* Para Erlang B */
 int ocupados_actuales;
 
-/* Punteros a archivos */
 FILE *parametros, *resultados;
 
 /* Prototipos de funciones */
@@ -71,7 +53,7 @@ void reporte(void);
 double exponencial(double media);
 double uniforme(void);
 double erlangB(double A, int N);
-double erlangC(double A, int s);   // Probabilidad teórica de espera para Erlang C
+double erlangC(double A, int s);
 
 int main(void)
 {
@@ -83,10 +65,8 @@ int main(void)
         exit(1);
     }
     
-    /* Entrada esperada en param.txt: 
-       t_interarribo_promedio t_servicio_promedio num_llegadas_requeridas num_servidores tipo_simulacion 
-       (Para Erlang B, tipo_simulacion debe ser 0; para Erlang C, tipo_simulacion debe ser 1)
-    */
+    /* Lectura de parámetros desde param.txt:
+       t_interarribo_promedio t_servicio_promedio num_llegadas_requeridas num_servidores tipo_simulacion */
     fscanf(parametros, "%lf %lf %d %d %d",
            &t_interarribo_promedio, &t_servicio_promedio, &num_llegadas_requeridas, &num_servidores, &tipo_simulacion);
     
@@ -100,8 +80,7 @@ int main(void)
     else
         fprintf(resultados, "Tipo de Simulación: Erlang C (Sistema de Colas)\n\n");
     
-    /* Semilla para el generador de números aleatorios */
-    srand((unsigned) time(NULL));
+    /* No se necesita srand() ya que usamos lcgrand */
     
     /* Inicializar contadores globales */
     total_llegadas = 0;
@@ -129,30 +108,23 @@ int main(void)
 }
 
 /*-------------------------------------------------------
-  Simulación para Erlang B (sistema de pérdida) usando tiempos fijos de llegada.
-  En este enfoque se genera cada tiempo de llegada de forma independiente.
-  Antes de procesar una llegada, se "avanza" la simulación procesando todas las salidas que ocurran
-  estrictamente antes de esa llegada.
-  (Una llamada que llega se pierde si todos los servidores están ocupados en su tiempo programado.)
+  Simulación para Erlang B (sistema de pérdida).
 ---------------------------------------------------------*/
 void simularErlangB(void)
 {
-    /* Asignar e inicializar los tiempos de salida para cada servidor */
     proxima_salida = (double *) malloc(num_servidores * sizeof(double));
     for (int i = 0; i < num_servidores; i++) {
         proxima_salida[i] = INFINITY_TIME;
     }
     ocupados_actuales = 0;
     
-    double ultimo_tiempo_evento = 0.0;  // Para el cálculo del promedio de ocupación
+    double ultimo_tiempo_evento = 0.0;
     double tiempo_llegada = 0.0;
     
     for (int i = 0; i < num_llegadas_requeridas; i++) {
-        /* Generar el siguiente tiempo de llegada */
         double interarribo = exponencial(t_interarribo_promedio);
         tiempo_llegada += interarribo;
         
-        /* Procesar todas las salidas que ocurran estrictamente antes de esta llegada */
         while (1) {
             double min_salida = INFINITY_TIME;
             int indice_min_salida = -1;
@@ -163,11 +135,9 @@ void simularErlangB(void)
                 }
             }
             if (min_salida < tiempo_llegada) {
-                /* Avanzar el tiempo de simulación al tiempo de salida */
                 tiempo_simulacion = min_salida;
                 area_ocupada += ocupados_actuales * (tiempo_simulacion - ultimo_tiempo_evento);
                 ultimo_tiempo_evento = tiempo_simulacion;
-                /* Procesar la salida: liberar el servidor */
                 proxima_salida[indice_min_salida] = INFINITY_TIME;
                 ocupados_actuales--;
             } else {
@@ -175,16 +145,13 @@ void simularErlangB(void)
             }
         }
         
-        /* Ahora procesar el evento de llegada en el tiempo "tiempo_llegada" */
         tiempo_simulacion = tiempo_llegada;
         area_ocupada += ocupados_actuales * (tiempo_simulacion - ultimo_tiempo_evento);
         ultimo_tiempo_evento = tiempo_simulacion;
         total_llegadas++;
         
         if (ocupados_actuales < num_servidores) {
-            /* Hay un servidor libre: se atiende la llamada */
             llamadas_atendidas++;
-            /* Encontrar un servidor libre (donde proxima_salida es INFINITY_TIME) */
             int indice_libre = -1;
             for (int j = 0; j < num_servidores; j++) {
                 if (proxima_salida[j] == INFINITY_TIME) {
@@ -195,12 +162,10 @@ void simularErlangB(void)
             proxima_salida[indice_libre] = tiempo_simulacion + exponencial(t_servicio_promedio);
             ocupados_actuales++;
         } else {
-            /* Todos los servidores están ocupados en el tiempo de llegada: se bloquea la llamada */
             llamadas_bloqueadas++;
         }
     }
     
-    /* Procesar las salidas restantes (para el cálculo del área) */
     while (ocupados_actuales > 0) {
         double min_salida = INFINITY_TIME;
         int indice_min_salida = -1;
@@ -219,8 +184,7 @@ void simularErlangB(void)
 }
 
 /*-------------------------------------------------------
-  Simulación para Erlang C (sistema de colas) usando programación de eventos estándar.
-  (Este es esencialmente el código original para Erlang C.)
+  Simulación para Erlang C (sistema de colas).
 ---------------------------------------------------------*/
 void simularErlangC(void)
 {
@@ -228,17 +192,14 @@ void simularErlangC(void)
     int num_en_cola = 0;
     double cola_espera[LIMITE_COLA];
     
-    /* Programar la primera llegada */
     proxima_llegada = tiempo_simulacion + exponencial(t_interarribo_promedio);
     
-    /* Asignar e inicializar los tiempos de salida para cada servidor */
     proxima_salida = (double *) malloc(num_servidores * sizeof(double));
     for (int i = 0; i < num_servidores; i++) {
         proxima_salida[i] = INFINITY_TIME;
     }
     
     while (llamadas_atendidas < num_llegadas_requeridas) {
-        /* Determinar el siguiente evento */
         double min_tiempo = proxima_llegada;
         int tipo_evento = EVENTO_LLEGADA;
         int servidor_evento = -1;
@@ -251,13 +212,11 @@ void simularErlangC(void)
         }
         tiempo_simulacion = min_tiempo;
         double tiempo_desde_ultimo_evento = tiempo_simulacion - ultimo_tiempo_evento;
-        /* (Se pueden agregar cálculos del área de forma similar si se desea) */
         ultimo_tiempo_evento = tiempo_simulacion;
         
         if (tipo_evento == EVENTO_LLEGADA) {
             total_llegadas++;
             proxima_llegada = tiempo_simulacion + exponencial(t_interarribo_promedio);
-            /* Procesar la llegada: si hay un servidor libre, iniciar el servicio; de lo contrario, encolar */
             int servidor_libre = -1;
             for (int i = 0; i < num_servidores; i++) {
                 if (proxima_salida[i] == INFINITY_TIME) {
@@ -281,7 +240,7 @@ void simularErlangC(void)
             if (num_en_cola > 0) {
                 double tiempo_llegada_evento = cola_espera[0];
                 for (int i = 0; i < num_en_cola - 1; i++) {
-                    cola_espera[i] = cola_espera[i+1];
+                    cola_espera[i] = cola_espera[i + 1];
                 }
                 num_en_cola--;
                 tiempo_total_espera += (tiempo_simulacion - tiempo_llegada_evento);
@@ -295,7 +254,7 @@ void simularErlangC(void)
 }
 
 /*-------------------------------------------------------
-  Reportar los resultados de la simulación.
+  Reporte de los resultados de la simulación.
 ---------------------------------------------------------*/
 void reporte(void)
 {
@@ -309,7 +268,6 @@ void reporte(void)
         double probabilidad_bloqueo = (double) llamadas_bloqueadas / total_llegadas;
         fprintf(resultados, "Probabilidad de Bloqueo (Simulada): %f\n", probabilidad_bloqueo);
         
-        /* Calcular la carga ofrecida A = λ/μ, donde λ = 1/t_interarribo_promedio y μ = 1/t_servicio_promedio */
         double lambda = 1.0 / t_interarribo_promedio;
         double mu = 1.0 / t_servicio_promedio;
         double A = lambda / mu;
@@ -325,11 +283,9 @@ void reporte(void)
         fprintf(resultados, "Probabilidad de Espera (Simulada):   %f\n", probabilidad_espera_sim);
         fprintf(resultados, "Tiempo Promedio de Espera:             %f\n", tiempo_espera_promedio);
         
-        /* Calcular la carga ofrecida A = λ/μ para Erlang C también */
         double lambda = 1.0 / t_interarribo_promedio;
         double mu = 1.0 / t_servicio_promedio;
         double A = lambda / mu;
-        /* Calcular la probabilidad teórica de espera usando la fórmula de Erlang C */
         double probabilidad_espera_teorica = erlangC(A, num_servidores);
         fprintf(resultados, "Probabilidad de Espera (Teórica Erlang C):   %f\n", probabilidad_espera_teorica);
     }
@@ -345,16 +301,16 @@ double exponencial(double media)
 }
 
 /*-------------------------------------------------------
-  Generador Uniforme(0,1) usando rand()
+  Generador Uniforme(0,1) usando lcgrand.
 ---------------------------------------------------------*/
 double uniforme(void)
 {
-    /* Retorna un número estrictamente entre 0 y 1 */
-    return (((double) rand() + 1.0) / ((double) RAND_MAX + 2.0));
+    // Utiliza lcgrand con stream 0 para generar números uniformes
+    return (double) lcgrand(0);
 }
 
 /*-------------------------------------------------------
-  Calcular la probabilidad de bloqueo de Erlang B usando recursión.
+  Cálculo de la probabilidad de bloqueo (Erlang B) usando recursión.
 ---------------------------------------------------------*/
 double erlangB(double A, int N)
 {
@@ -366,26 +322,23 @@ double erlangB(double A, int N)
 }
 
 /*-------------------------------------------------------
-  Calcular la probabilidad teórica de espera para una cola M/M/s usando la fórmula de Erlang C.
+  Cálculo de la probabilidad teórica de espera (Erlang C).
 ---------------------------------------------------------*/
 double erlangC(double A, int s)
 {
     if (A >= s) {
-        /* El sistema es inestable; retornar 1 ya que todas las llegadas esperarán */
-        return 1.0;
+        return 1.0;  // Sistema inestable
     }
     
     double suma = 0.0;
-    double termino = 1.0;  // Término para n = 0: A^0 / 0! = 1
+    double termino = 1.0;
     int n;
     for (n = 0; n < s; n++) {
         if (n > 0)
-            termino *= A / n;  // Calcular A^n/n! de forma recursiva
+            termino *= A / n;
         suma += termino;
     }
-    /* "termino" ahora tiene A^(s-1)/(s-1)!; calcular el término para n = s */
-    double termino_s = termino * (A / s);  // A^s/s!
-    
+    double termino_s = termino * (A / s);
     double PC = termino_s * (s / (s - A)) / (suma + termino_s * (s / (s - A)));
     return PC;
 }
