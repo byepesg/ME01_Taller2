61dec92c97a85b3c704c032f0348dcf5df0fdf3e
byepesg <byepesg@unal.edu.co>
2025-02-21 18:47:37 -0500

    Feature: First stable version

--- MM1Exponencial/SistemaDeColas.cpp
+++ MM1Exponencial/SistemaDeColas.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "lcgrand.cpp"  /* Encabezado para el generador de numeros aleatorios */
+#include "lcgrand.h"  /* Encabezado para el generador de numeros aleatorios */
 
 #define LIMITE_COLA 100  /* Capacidad maxima de la cola */
 #define OCUPADO      1  /* Indicador de Servidor Ocupado */
